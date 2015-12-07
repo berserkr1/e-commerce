b11eaabba0845647f66547ddd38c0c1c8b64ae27
Somewher <mickaelkael12@hotmail.com>
2015-12-07 15:16:50 +0100

    category class

---
+++ models/Category.class.php
@@ -0,0 +1,52 @@
+<?php 
+class Category
+{
+	private $id;
+	private $name;
+	private $description;
+	private $img;
+
+	public function __construct($db)
+	{
+		$this->db = $db;
+	}
+
+	public function getId()
+	{
+		return $this -> id;
+	}
+	public function getName()
+	{
+		return $this -> name;
+	}
+	public function getDescription()
+	{
+		return $this -> description;
+	}
+	public function getImg()
+	{
+		return $this -> img;
+	}
+
+	public function setId()
+	{
+		$this -> id = $id;
+		return true; 
+	}
+	public function setName()
+	{
+		$this -> name = $name;
+		return true;
+	}
+	public function setDescription()
+	{
+		$this -> description = $description;
+		return true;
+	}
+	public function setImg()
+	{
+		$this -> img = $img;
+		return true;
+	}
+}
+ ?>
