f86c5baa94bfc81e6e4e87bee1b8112225ea811b
Somewher <mickaelkael12@hotmail.com>
2015-12-09 16:51:33 +0100

    category

--- models/CategoryManager.class.php
+++ models/CategoryManager.class.php
@@ -9,92 +9,81 @@ class CategoryManager
 	}
 
 	public function create($name, $description, $img)
+	{
+		$category = new Category($this->db);
+		$category->setName($name);
+		$category->setDescription($description);
+		$category->setImg($img);
+		if (count($errors) == 0)
 		{
-			$category = new Category($this->db);
-			$valide	= $category->setName($name);
-			$errors = array();
-			$errors[] = $category->setName($name);
-			$errors[] = $category->setDescription($description);
-			$errors[] = $category->setImg($img);			
-			$errors = array_filter($errors, function($val)
-			{
-				return $val !== true;
-			});
-
-			if (count($errors) == 0)
-			{
-				$name = mysqli_escape_string($this->db, $category->getName());
-				$description = mysqli_escape_string($this->db, $category->getDescription());
-				$img = mysqli_escape_string($this->db, $category->getImg());
-				$query = "INSERT INTO category (name, description, img) VALUES('".$name."','".$description."','".$img."')";
-				$res = $this->db->exec($query);
-				if ($res)
-				{
-					$id = $this->db->lastInsertId();
-					if ($id)
-					{
-						return $this->findById($id);
-					}
-					else
-					{
-						throw new Exception ("Erreur interne du serveur");
-					}
-				}				
-			}
-			else
-			{
-				return $errors;
-			}		
-		}
-
-	public function read($n = 0)
-		{
-			$n = intval($n);		
-			if ($n > 0)
-			{
-				$query = 'SELECT * FROM category ORDER BY `name` ASC LIMIT '.$n;
-			}
-			else
-			{
-				$query = 'SELECT * FROM section ORDER BY `name` ASC';
-			}
-			$res = $this->db->query($query);
+			$name = $this->db->quote($category->getName());
+			$description = $this->db->quote($category->getDescription());
+			$img = $this->db->quote($category->getImg());
+			$query = "INSERT INTO category (name, description, img) VALUES(".$name.",".$description.",".$img.")";
+			$res = $this->db->exec($query);
 			if ($res)
 			{
-				$category = array();
-				while ($category = $res->fetchObject("Category", array($this->db)))
+				$id = $this->db->lastInsertId();
+				if ($id)
 				{
-					$category[] = $category;
+					return $this->findById($id);
 				}
+				else
+				{
+					throw new Exception ("Erreur interne du serveur");
+				}
+			}				
+		}	
+	}
+
+	public function read($n = 0)
+	{
+	 	$n = intval($n);		
+	 	if ($n > 0)
+	 	{
+	 		$query = 'SELECT * FROM category ORDER BY `name` ASC LIMIT '.$n;
+	 	}
+	 	else
+	 	{
+	 		$query = 'SELECT * FROM section ORDER BY `name` ASC';
+	 	}
+	 	$res = $this->db->query($query);
+	 	if ($res)
+	 	{
+	 		$category = array();
+	 		while ($category = $res->fetchObject("Category", array($this->db)))
+	 		{
+	 			$category[] = $category;
+	 		}
+	 		return $category;
+	 	}
+	 	else
+	 	{
+	 		throw new Exception ("Database error");
+	 	}
+	}
+
+	public function findById($id)
+	{
+		$id	= intval($id);
+		$query = "SELECT * FROM category WHERE id=".$id;
+		$res = $this->db->query($query);			
+		if ($res)
+		{
+			$category = $res->fetchObject("Category", array($this->db));
+			if ($category)
+			{
 				return $category;
 			}
 			else
 			{
-				return 'Database error';
+				throw new Exception("No match");
 			}
 		}
-
-	public function findByIdCategory($id)
+		else
 		{
-			$id	= intval($id);
-			$query = "SELECT * FROM category WHERE id=".$id;
-			$res = $this->db->query($query);			
-			if ($res)
-			{
-				$category = $res->fetchObject("Category", array($this->db));
-				if ($category)
-				{
-					return $category;
-				}
-				else
-				{
-					return "No match";
-				}
-			}
-			else
-			{
-				return "Internal Server Error";
-			}
+			throw new Exception("Internal Server Error");
 		}
+	}
 }
  ?>
