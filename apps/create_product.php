6ddc43d04639088c1a3988a78c1d4322f76c2fb4
Somewher <mickaelkael12@hotmail.com>
2015-12-09 09:50:45 +0100

    product manager

--- apps/create_product.php
+++ apps/create_product.php
@@ -1,3 +1,7 @@
 <?php 
+$productManager = new ProductManager($db);
+$sub_categoryManager = new SubCategoryManager($db);
+
+$sub_category = $sub_categoryManager->read();
 require('views/create_product.phtml');
  ?>
--- models/ProductManager.class.php
+++ models/ProductManager.class.php
@@ -9,17 +9,27 @@ class ProductManager
 	}
 
 	public function create(SubCategory $sub_category, $name, $description, $price, $img, $stock)
-	$product = new Product($this->db);
-	$errors[] = $product->setSub_category($sub_category);
-	$errors[] = $product->setName($name);
-	$errors[] = $product->setDescription($description);
-	$errors[] = $product->setPrice($price);
-	$errors[] = $product->setImg($img);
-	$errors[] = $product->setStock($stock);
-	$errors = array_filter($errors, function ($val) {
-					return $val !== true;
-				});
-	if (count($errors) == 0)
+	{
+		$errors = array();		
+		$product = new Product($this->db);
+		try
+		{
+			$product->setSub_category($sub_category);
+			$product->setName($name);
+			$product->setDescription($description);
+			$product->setPrice($price);
+			$product->setImg($img);
+			$product->setStock($stock);
+		}
+		catch (Exception $e)
+		{
+			$errors[] = $e->getProduct;
+		}	
+		$errors = array_filter($errors, function ($val) 
+		{
+			return $val !== true;
+		});
+		if (count($errors) == 0)
 		{
 			$idSub_category = intval($product->getIdSub_category());
 			$name = mysqli_escape_string($this->db, $product->getName());
@@ -28,15 +38,23 @@ class ProductManager
 			$img = mysqli_escape_string($this->db, $product->getImg());
 			$stock = mysqli_escape_string($this->db, $product->getStock());
 			$query = "INSERT INTO product(id_sub_category, name, description, price, img, stock) VALUES('" . $idSub_category . "', '" . $name . "', '" . $description . "', '" . $price . "', '" . $img . "', '" . $stock . "')";
-			$data = mysqli_query($this->db, $query);
-			else
+			$res = mysqli_query($this->db->exec($query));
+			if ($res)
 			{
-				return "DB connect error";
+				$id = $this->db->lastInsertId();
+				if ($id)
+				{
+					return $this->findById($id);
+				}
+				else
+				{
+					return "Internal Server error";
+				}
 			}
 		}
 		else
 		{
 			return $errors;
 		}	
+	}
 }
- ?>
