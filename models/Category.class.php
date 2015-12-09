54efce5b18224a6e5b4ad2b8e1041498fe719edc
Somewher <mickaelkael12@hotmail.com>
2015-12-09 13:26:53 +0100

    category class

--- models/Category.class.php
+++ models/Category.class.php
@@ -1,13 +1,15 @@
 <?php 
-class Category
+class Category extends SubCategoryManager
 {
 	private $id;
 	private $name;
 	private $description;
 	private $img;
+	private $db;
 
 	public function __construct($db)
 	{
+		parent::__construct($db);
 		$this->db = $db;
 	}
 
@@ -27,26 +29,53 @@ class Category
 	{
 		return $this->img;
 	}
-
-	public function setId()
+	
+	public function setName($name)
 	{
-		$this->id = $id;
-		return true; 
+		if (strlen($name) > 1 && strlen($name) < 32)
+		{
+			$this -> name = $name;
+			return true;
+		}
+		else
+		{
+			return "Invalid name";
+		}
 	}
-	public function setName()
+	public function setDescription($description)
 	{
-		$this->name = $name;
-		return true;
+		if (strlen($description) > 1 && strlen($description) < 512)
+		{
+			$this -> description = $description;
+			return true;
+		}
+		else
+		{
+			return "Invalid description";
+		}
 	}
-	public function setDescription()
+	public function setImg($img)
 	{
-		$this->description = $description;
-		return true;
-	}
-	public function setImg()
-	{
-		$this->img = $img;
-		return true;
+		if ($image_proprietes = @getimagesize($img))
+		{
+			if ($image_proprietes[0] > 400 || $image_proprietes[1] > 400)
+			{
+				return "Invalid image dimensions (max 400x400 px)";
+			}
+			else if (@filesize($image) > 1e6)
+			{
+				return "Invalid image size (max 25 kB)";
+			}
+			else
+			{
+				$this -> image = $image;
+				return true;
+			}
+		}
+		else
+		{
+			return "Invalid filetype";
+		}
 	}
 }
  ?>
