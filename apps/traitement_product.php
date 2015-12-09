a7bd92de76b01ef31b51a28630fa2bcd2c37739d
berserkr1 <g.coirier@epmistes.net>
2015-12-09 16:14:59 +0100

    maj traitement_product

--- apps/traitement_product.php
+++ apps/traitement_product.php
@@ -4,9 +4,9 @@ if (isset($_POST['action'])) {
 		if (isset ($_POST['create_product'])) {
 			$create_product = $_POST['create_product'];
 			$productManager = new ProductManager($db);
-			$sub_categoryManager = new SubCategoryManager($db);
-			$sub_category = $sub_categoryManager->readById(intval($_GET['id']));
-			$res = $productManager->create($sub_category, $_POST['create_product']);
+			$subCategoryManager = new SubCategoryManager($db);
+			$subCategory = $subCategoryManager->readById(intval($_GET['id']));
+			$res = $productManager->create($subCategory, $_POST['create_product']);
 			if (is_array($res))
 			{
 				$errors = $res;
@@ -19,7 +19,7 @@ if (isset($_POST['action'])) {
 			else
 			{
 				$_SESSION['success'] = "Produit créé avec succès";
-				header('Location: ?page=product&id=' . $res->getProduct()->getId());
+				header('Location: ?page=product&id='.$res->getProduct()->getId());
 				exit;
 			}
 		}
