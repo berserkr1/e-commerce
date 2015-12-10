214da39946c763ea7f6bebbe61cda0fadfc8c58a
Somewher <mickaelkael12@hotmail.com>
2015-12-10 09:49:32 +0100

    menu

--- apps/sub_category.php
+++ apps/sub_category.php
@@ -1,3 +1,19 @@
 <?php 
-require('views/sub_category.phtml');
+$categoryManager = new CategoryManager($db);
+try
+{
+	$category = $categoryManager->findById($_GET['id']);
+}
+catch (Exception $e)
+{
+	$category = $e->getMessage();
+}
+if (!is_string($category))
+{
+	require('views/category.phtml');
+}
+else
+{
+	echo ("Nothing to show");
+}
  ?>
