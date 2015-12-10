490d7e6e23858a2f5426b27c4bc3097ca2113b87
Caille130208 <pascal.herry@sfr.fr>
2015-12-10 09:51:37 +0100

    lkhkj

--- apps/profil.php
+++ apps/profil.php
@@ -1,28 +1,6 @@
 <?php 
-$addressManager = new AddressManager($db);
-try
-{
-	$address = $addressManager -> findByIdUser($currentUser->getId());
-}
-catch (Exception $e)
-{
-	$address = $e->getMessage();
-}
-$ship_address=$ship_postal_code=$ship_city=$ship_region=$ship_country=$bill_address=$bill_postal_code=$bill_city=$bill_region=$bill_country="";
-if (is_object($address))
-{
-	$ship_address=$address->getShipAddress();
-	$ship_postal_code=$address->getShipPostalCode();
-	$ship_city=$address->getShipCity();
-	$ship_region=$address->getShipRegion();
-	$ship_country=$address->getShipCountry();
-	$bill_address=$address->getBillAddress();
-	$bill_postal_code=$address->getBillPostalCode();
-	$bill_city=$address->getBillCity();
-	$bill_region=$address->getBillRegion();
-	$bill_country=$address->getBillCountry();
 
-}
+$ship_address=$ship_postal_code=$ship_city=$ship_region=$ship_country=$bill_address=$bill_postal_code=$bill_city=$bill_region=$bill_country="";
 
 require('views/profil.phtml');
 ?>
